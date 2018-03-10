a82375e372614b5daba9921b0cb246ac73a8c451
Douglas McCandless <mccandlessd@gmail.com>
2018-03-09 19:34:38 -0500

    continue basic layout in index.html & style.css, build out of basic object-oriented program functionality in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,102 @@
 $(document).ready(function(){
 
-//variables
+	$("#rubyButton").click(function(){
+		crystalsGame.ruby;
+	});
+	
 
-var targetSum = 0;
-var playerSum = 0;
+	$("#diamondButton").click(function(){
+		crystalsGame.diamond;
+	});
+	
 
-var valueAddToRuby = 0;
-var valueAddToDiamond = 0;
-var valueAddToTopaz = 0;
-var valueAddToEmerald = 0;
+	$("#topazButton").click(function(){
+		crystalsGame.topaz;
+	});
+	
 
-var winsCounter = 0;
-var lossesCounter = 0;
-
-//functions/objects
-
-function initGame(){
-// reset variables
-
-//obtain random values of targetSum and valueAdders
-
-targetSum = Math.floor(Math.random() * 101); //offset needed
-
-valueAddToRuby = Math.floor(Math.random() * 12); //offset needed
-valueAddToDiamond = Math.floor(Math.random() * 12); //offset needed
-valueAddToTopaz = Math.floor(Math.random() * 12); //offset needed
-valueAddToEmerald = Math.floor(Math.random() * 12); //offset needed
-
-
-
-}
-
-function playGame(){
-
-$("#rubyButton").click(function(){
-	playerSum += valueAddToRuby;
-});
-
-$("#diamondButton").click(function(){
-	playerSum += valueAddToDiamond;
-});
-
-$("#topazButton").click(function(){
-	playerSum += valueAddToTopaz;
-});
-
-$("#emeraldButton").click(function(){
-	playerSum += valueAddToEmerald;
+	$("#emeraldButton").click(function(){
+		crystalsGame.emerald;
+	});
+	
 });
 
 
-if(playerSum === targetSum){
+var crystalsGame = {
+
+	targetSum: 0,
+	playerSum: 0,
+
+	winsCounter: 0,
+	lossesCounter: 0,
+
+	valueAddFromRuby: 0,
+	valueAddFromDiamond: 0,
+	valueAddFromTopaz: 0,
+	valueAddFromEmerald: 0,
+
+	resetInitValues: function(){
+		crystalsGame.targetSum = 0;
+		crystalsGame.playerSum = 0;
+
+		crystalsGame.winsCounter = 0;
+		crystalsGame.lossesCounter = 0;
+
+		crystalsGame.valueAddFromRuby = 0;
+		crystalsGame.valueAddFromDiamond = 0;
+		crystalsGame.valueAddFromTopaz = 0;
+		crystalsGame.valueAddFromEmerald = 0;
+
+		$("#numGenerated").text("");
+		$("#sumNumsEntered").text("");
+
+	},
+
+	setInitValues: function(){
+		crystalsGame.targetSum = Math.floor(Math.random() * 101) + 19;
+		console.log(targetSum);
+
+		crystalsGame.valueAddFromRuby = Math.floor(Math.random() * 12) + 1;
+		console.log(valueAddToRuby);
+		crystalsGame.valueAddFromDiamond = Math.floor(Math.random() * 12) + 1;
+		console.log(valueAddToDiamond);
+		crystalsGame.valueAddFromTopaz = Math.floor(Math.random() * 12) + 1;
+		console.log(valueAddToTopaz);
+		crystalsGame.valueAddFromEmerald = Math.floor(Math.random() * 12) + 1;
+		console.log(valueAddToEmerald);
+
+		$("#numGenerated").text(crystalsGame.targetSum);
+
+
+	},
+
+
+	ruby: function(){
+		crystalsGame.playerSum += crystalsGame.valueAddFromRuby;
+	},
+
+	diamond: function(){
+		crystalsGame.playerSum += crystalsGame.valueAddFromDiamond;
+	},
+
+	topaz: function(){
+		crystalsGame.playerSum += crystalsGame.valueAddFromTopaz;
+	},
+
+	emerald: function(){
+		crystalsGame.playerSum += crystalsGame.valueAddFromEmerald;
+	},
+
+}; //end of crystalsGame object
+
+
+
+//$("#sumNumsEntered").text(crystalsGame.playerSum);
+
+
+
+
+/*if(playerSum === targetSum){
 	alert("Winner");
 	winsCounter++;
 	wrapUpGame();
@@ -59,30 +105,9 @@ if(playerSum === targetSum){
 		lossesCounter++;
 		wrapUpGame();
 	} else {
-		[next increment cycle]
-	}
+		continueGame();
+	}*/
+//note: continueGame() is a placeholder function
 
 
 
-}
-
-function wrapUpGame(){
-
-}
-
-
-//processing/calls
-
-initGame();
-
-
-
-
-
-
-
-
-
-
-
-});
