7b282b48f57499184be7ac00a367d9969724feda
Douglas McCandless <mccandlessd@gmail.com>
2018-03-14 12:08:25 -0400

    in game.js, dropped unnecessary empty else branch from end of determineContest(), tweaked offset in random generation of targetSum to ensure range of variables generated will extend up to 120, inserted explanatory comments throughout game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+//set up the crystals buttons and set the initial value variables of the game
+
 $(document).ready(function(){
 
 	$("#rubyButton").click(function(){
@@ -23,10 +25,11 @@ $(document).ready(function(){
 	
 });
 
-
+//here's the game object
 
 var crystalsGame = {
 
+//initial variables
 	targetSum: 0,
 	playerSum: 0,
 
@@ -37,6 +40,10 @@ var crystalsGame = {
 	valueAddFromDiamond: 0,
 	valueAddFromAmber: 0,
 	valueAddFromEmerald: 0,
+
+//after a game has been played
+//reset of variables and displays of target number and 
+//total number entered by player
 
 	resetInitValues: function(){
 		crystalsGame.targetSum = 0;
@@ -54,10 +61,17 @@ var crystalsGame = {
 
 	},
 
+//set the random values of target number and values of crystals
+
 	setInitValues: function(){
 
-		crystalsGame.targetSum = Math.floor(Math.random() * 101) + 19;
+//the target number which the player tries to match
+//is set for this game with a random number from 19 to 120
+
+		crystalsGame.targetSum = Math.floor(Math.random() * 102) + 19;
 		console.log(crystalsGame.targetSum);
+
+//each crystal is set for this game with random value from 1 to 12
 
 		crystalsGame.valueAddFromRuby = Math.floor(Math.random() * 12) + 1;
 		console.log(crystalsGame.valueAddFromRuby);
@@ -68,11 +82,16 @@ var crystalsGame = {
 		crystalsGame.valueAddFromEmerald = Math.floor(Math.random() * 12) + 1;
 		console.log(crystalsGame.valueAddFromEmerald);
 
+//display the target number for player to play against
+
 		$("#numGenerated").text(crystalsGame.targetSum);
 
 
 	},
 
+//event listeners for crystals: they increment player's sum,
+//display the increment in player's sum, and call 
+//determine contest function to see if game is won or lost
 
 	ruby: function(){
 		crystalsGame.playerSum += crystalsGame.valueAddFromRuby;
@@ -98,6 +117,17 @@ var crystalsGame = {
 		crystalsGame.determineContest(crystalsGame.targetSum, crystalsGame.playerSum);
 	},
 
+//tests if game is won or lost:
+//in both win or lose case, final player's sum is displayed again
+//as part of tactic to make certain alert does not conflict with such display;
+//alert is given of win or loss,
+//and alert is delayed by 2 seconds as other part of tactic
+//to make certain alert does not conflict with final display of player's sum;
+//win or loss counter is incremented,
+//and wrap up game function is called;
+//if no decision yet in game, nothing is done by this function
+//and crystal buttons continue to assert control when
+//they are pressed and generate events
 
 	determineContest: function(tSum, pSum){
 		if(tSum === pSum){
@@ -110,11 +140,12 @@ var crystalsGame = {
 			setTimeout(function(){alert ("Player Loses");}, 2000);
 			crystalsGame.lossesCounter++;
 			crystalsGame.wrapUpGame();
-		} else {
-			//I don't like having an empty branch here, but it works.
 		}
 
 	},
+
+//displays cumulative wins and losses for player as of this end of game
+//after 2 second delay, starts a new game
 
 	wrapUpGame: function(){
 		$("#winsCounted").text("Wins: " + crystalsGame.winsCounter);
@@ -122,7 +153,8 @@ var crystalsGame = {
 		setTimeout(crystalsGame.resetInitValues, 2000);
 	}
 
-}; //end of crystalsGame object
+}; 
+//end of crystalsGame object
 
 
 
